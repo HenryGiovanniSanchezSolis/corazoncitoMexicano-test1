3c5319842d0fbf543e603a788a63230d5b634e29
sshenrygiovanni <sshenrygiovanni@gmail.com>
2024-08-28 20:23:44 -0600

    Con archivos de despliegue

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Corazoncito Mexicano develop2'),
+          child: Text('Corazoncito Mexicano en proceso...'),
         ),
       ),
     );
